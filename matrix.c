46cabaf926706758c0680b67e10edead0850de8e
Saif eddine El hanoune <sel-hano@e1r5p6.1337.ma>
2023-04-11 02:59:37 +0000

    ..

---
+++ matrix.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to allocate memory for a matrix of integers
+int** allocateMatrix(int rows, int cols) {
+    int** matrix = (int**) malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (int*) malloc(cols * sizeof(int));
+    }
+    return matrix;
+}
+
+// Function to initialize a matrix with values
+void initializeMatrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = i * cols + j;
+        }
+    }
+}
+
+// Function to print the contents of a matrix
+void printMatrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Function to free the memory allocated for a matrix
+void freeMatrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+int main() {
+    int rows = 3;
+    int cols = 4;
+
+    // Declare a triple pointer to hold a double pointer
+    int*** triplePointer;
+
+    // Allocate memory for the double pointer
+    triplePointer = (int***) malloc(sizeof(int**));
+
+    // Allocate memory for the matrix using the double pointer
+    *triplePointer = allocateMatrix(rows, cols);
+
+    // Initialize the matrix with values
+    initializeMatrix(*triplePointer, rows, cols);
+
+    // Print the matrix
+    printf("Matrix:\n");
+    printMatrix(*triplePointer, rows, cols);
+
+    // Free the memory allocated for the matrix
+    freeMatrix(*triplePointer, rows);
+
+    // Free the memory allocated for the double pointer
+    free(triplePointer);
+
+    return 0;
+}
+
